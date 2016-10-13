ae53d223de7881e5ea29ce1ade43e202f715011f
Stephan Tabor <stephantabor@users.noreply.github.com>
2016-10-13 17:28:13 -0400

    Update dependencies.sh

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -3,7 +3,7 @@ run_if_present() {
   local has_script=$(read_json "$BUILD_DIR/package.json" ".scripts[\"$script_name\"]")
   if [ -n "$has_script" ]; then
     echo "Running $script_name"
-    npm run "$script_name" --if-present
+    yarn run "$script_name" --if-present
   fi
 }
 
@@ -13,12 +13,12 @@ install_node_modules() {
   if [ -e $build_dir/package.json ]; then
     cd $build_dir
 
-    if [ -e $build_dir/npm-shrinkwrap.json ]; then
-      echo "Installing node modules (package.json + shrinkwrap)"
+    if [ -e $build_dir/yarn.lock ]; then
+      echo "Installing node modules (package.json + yarn.lock)"
     else
       echo "Installing node modules (package.json)"
     fi
-    npm install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
+    yarn install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
   else
     echo "Skipping (no package.json)"
   fi
@@ -30,13 +30,13 @@ rebuild_node_modules() {
   if [ -e $build_dir/package.json ]; then
     cd $build_dir
     echo "Rebuilding any native modules"
-    npm rebuild 2>&1
-    if [ -e $build_dir/npm-shrinkwrap.json ]; then
-      echo "Installing any new modules (package.json + shrinkwrap)"
+    # npm rebuild 2>&1
+    if [ -e $build_dir/yarn.lock ]; then
+      echo "Installing any new modules (package.json + yarn.lock)"
     else
       echo "Installing any new modules (package.json)"
     fi
-    npm install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
+    yarn install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
   else
     echo "Skipping (no package.json)"
   fi
